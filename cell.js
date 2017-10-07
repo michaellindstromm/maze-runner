790dadf2a5b24328b20d509df0524e618a736081
Michael Lindstrom <michaellindstr@gmail.com>
2017-10-07 13:34:39 -0500

    commented code

--- cell.js
+++ cell.js
@@ -1,33 +1,61 @@
+
+// CELL CONSTRUCTOR
 function Cell(i, j) {
+
+    // SETS THE I and J WHICH IS THE X and Y OF THE CORRESPONDING ROW AND COLUMN
+    // ie. (0, 0) IS THE TOP LEFT CELL
     this.i = i;
     this.j = j;
+
+    // SETS THE WALLS OF THE CURRENT CELL ALL TO TRUE CREATING A GRID
+    // WALLS[0] = TOP
+    // WALLS[1] = RIGHT
+    // WALLS[2] = BOTTOM
+    // WALLS[3] = LEFT
     this.walls = [true, true, true, true];
+
+    // INITIALLY SET TO FALSE WHEN FIRST CREATING GRID
     this.visited = false;
 
+    //FUNCTION THAT CHECKS FOR THIS CELLS TOP, RIGHT, BOTTOM, and LEFT NEIGHBORS
     this.checkNeighbors = function () {
 
         var neighbors = [];
 
+        // SETS VARIABLES EACH OF THIS CELL'S NEIGHBORS
         var top = grid[index(i, j - 1)];
         var right = grid[index(i + 1, j)];
         var bottom = grid[index(i, j + 1)];
         var left = grid[index(i - 1, j)];
 
+        
+        // THE IF STATEMENTS BELOW CHECK TWO THINGS
+        // 1. IF THIS CELL HAS A NEIGHBOR (FOR EDGE CASES lol)
+        // 2. IF PARTICULAR NEIGHBOR FOR THIS CELL HAS ALREADY BEEN VISITED
+        // IF EVALUATED TO TRUE THE NEIGHBORS FOR THIS CELL GET PUSHED INTO THIS CELL'S NEIGHBORS ARRAY
+
+        // TOP
         if (top && !top.visited) {
 
             neighbors.push(top);
 
         }
+
+        // RIGHT
         if (right && !right.visited) {
 
             neighbors.push(right);
 
         }
+
+        // BOTTOM
         if (bottom && !bottom.visited) {
 
             neighbors.push(bottom);
 
         }
+
+        // LEFT
         if (left && !left.visited) {
 
             neighbors.push(left);
@@ -35,12 +63,14 @@ function Cell(i, j) {
         }
 
 
+        // IF THIS CELL HAS UNVISITED NEIGHBORS THIS RETURNS A RANDOM NEIGHBOR FOR THE CURRENT CELL TO GO TO NEXT
         if (neighbors.length > 0) {
 
             var r = floor(random(0, neighbors.length));
 
             return neighbors[r];
 
+        // OTHERWISE IT RETURNS UNDEFINED
         } else {
 
             return undefined;
@@ -49,6 +79,9 @@ function Cell(i, j) {
 
 
     }
+
+
+    // USED TO HIGHLIGHT THE CURRENT CELL
     this.highlight = function () {
         var x = this.i * w;
         var y = this.j * w;
@@ -58,31 +91,43 @@ function Cell(i, j) {
 
     }
 
+    // USED TO DRAW LINES FOR CELL BASED ON THIS CELLS WALLS ARRAY
     this.show = function () {
         var x = this.i * w;
         var y = this.j * w;
         stroke(255);
+
+        // IF THIS CELL'S WALLS ARRAY AT CERTAIN INDEX IS TRUE IT DRAWS THE CORRECT WALL ACCORDINGLY
+
+        // TOP
         if (this.walls[0]) {
 
             line(x, y, x + w, y);
 
         }
+
+        // RIGHT
         if (this.walls[1]) {
 
             line(x + w, y, x + w, y + w);
 
         }
+
+        // BOTTOM
         if (this.walls[2]) {
 
             line(x + w, y + w, x, y + w);
 
         }
+
+        // LEFT
         if (this.walls[3]) {
 
             line(x, y + w, x, y);
 
         }
 
+        // WHEN CELL IS VISITED IT DRAWS A RECTANGLE WITH NO BORDER SO ONLY CORRECT WALLS SHOW
         if (this.visited) {
 
             noStroke();
--- maze.js
+++ maze.js
@@ -1,23 +1,45 @@
+// TO KNOW WHICH CELL PLAYER IS IN
 var playerX = 0;
 var playerY = 0;
+
+// KEEP TRACK OF NUM OF COLS AND ROWS
 var cols, rows;
+
+// TO DEFINE DIMENTIONS OF ONE CELL
 var w = 20;
+
+// TO STORE CELL OBJECTS IN ORDER: LEFT - RIGHT & TOP - BOTTOM
 var grid = [];
+
+// KEEPS TRACK OF CURRENT CELL
 var current;
+
+// KEEPS TRACK OF VISITED CELLS WHEN GENERATING THE MAZE
 var stack = [];
+
+// STORES THE FURTHEST CELL TO MAXIMIZE CORRECT MAZE PATH
 var furthestCell;
+
+// USED TO TRACK WHICH CELL IS FURTHEST FROM START
 var stackLength = 0;
+
+// SET TO TRUE WHEN MAZE IS FINISHED GENERATING
 var mazeFinished = false;
 
+// SETUP IS A P5.JS FUNCTION
 function setup() {
 
-    createCanvas(200, 200);
+    createCanvas(300, 300);
 
     cols = floor(width / w);
     rows = floor(height / w);
 
+    // FOR TESTING TO SLOW DOWN FRAME RATE
     //frameRate(5);
 
+
+
+    // PUSHES A NEW CELL TO THE GRID
     for (var j = 0; j < rows; j++) {
 
         for (var i = 0; i < cols; i++) {
@@ -30,14 +52,18 @@ function setup() {
 
     }
 
+    // SETS CURRENT CELL TO TOP LEFT CORNER
     current = grid[0];
 
 }
 
+// DRAW IS A P5.JS FUNCTION
+// IT RUNS CONTINUOUSLY UNTIL noLopp() IS CALLED
 function draw() {
 
     background(100);
 
+    // CREATE GRID THAT GETS COVERED AS MAZE IS GENERATED
     for (var i = 0; i < grid.length; i++) {
 
         grid[i].show();
@@ -47,42 +73,57 @@ function draw() {
     current.visited = true;
 
     current.highlight();
-    // STEP 1
+    
+    // CALLS FUNCTION THAT SETS THE NEXT CELL THE GENERATOR WILL VISIT BASED ON THE UNVISITED CELLS AROUND THE CURRENT CELL
     var next = current.checkNeighbors();
 
     if (next) {
         
+
         next.visited = true;
         
-        // STEP 2
+        // PUSHES CURRENT CELL TO THE STACK FOR BACK TRACKING PURPOSES
         stack.push(current);
         
-        // STEP 3
+        // REMOVES WALLS BETWEEN CURRENT AND NEXT CELL
         removeWalls(current, next);
         
-        // STEP 4
+        // SETS CURRENT CELL TO THE NEXT CELL
         current = next;
         
+
+    // AS LONG AS THERE IS SOMETHING IN THE STACK THE GENERATOR WILL CONTINUE TO RUN
     } else if (stack.length > 0) {
         
+        // CHECKS TO SEE IF CURRENT CELL IS FURTHEST FROM STARTING POINT
         if (stack.length > stackLength) {
 
+            // SETS STACKLENGTH CHECKER VARIABLE
             stackLength = stack.length;
+
+            // SETS FURTHEST CELL TO CURRENT CELL
             furthestCell = current;
 
         }
         
+        // SETS CURRENT CELL TO MOST RECENT CELL IN STACK IN ORDER TO BACK TRACK
+        // THIS IS THE ENTIRE BACKTRACKING PART
         current = stack.pop();
         
+
+    // WHEN THERE IS NOTHING LEFT IN THE STACK 
+    // MEANING THE CURRENT CELL IS AT THE STARTING POSITION    
     } else {
 
-        endX = (furthestCell.i) * w;
-        endY = (furthestCell.j) * w;
+        // CREATES THE ENDING GOAL CELL FROM THE FURTHEST CELL
+        var endX = (furthestCell.i) * w;
+        var endY = (furthestCell.j) * w;
 
         fill(0, 255, 0, 100);
         rect(endX, endY, w, w);
 
 
+        // SETS VARIABLE TO TRUE SIGNALING THE MAZE HAS FINISHED GENERATING
         mazeFinished = true;
     }
 
@@ -93,32 +134,54 @@ function keyPressed() {
     var cI = current.i;
     var cJ = current.j;
     
+
+    // KEYPRESSES ONLY WORK ONCE THE MAZE HAS FINISHED GENERATING
     if (mazeFinished) {
 
+        // THESE IF STATEMENTS CHECK FOR FOUR THINGS 
+        // 1. WHICH KEY IS PRESSED (ie. LEFT, RIGHT, UP, or DOWN)
+        // 2. IF THE PLAYER IS AT THE EDGE OF THE CANVAS
+        // 3. IF THE NEXT CELL THE PLAYER IS ATTEMPTING TO GO TO HAS A WALL
+        // 4. IF THE CURRENT CELL THE PLAYER IS IN HAS A WALL
+
+        // LEFT ARROW
         if (keyCode === LEFT_ARROW && playerX !== 0 && (grid[index(cI - 1, cJ)].walls[1] !== true) && current.walls[3] !== true) {
             
             current = grid[(playerX - 1) + playerY * cols];
             playerX -= 1;
             
+        // RIGHT ARROW
         } else if (keyCode === RIGHT_ARROW && playerX + 1 !== cols && (grid[index(cI + 1, cJ)].walls[3] !== true) && current.walls[1] !== true ) {
 
             current = grid[(playerX + 1) + playerY * cols];
             playerX += 1;
-            
+          
+        // UP ARROW
         } else if (keyCode === UP_ARROW && playerY !== 0 && (grid[index(cI, cJ - 1)].walls[2] !== true) && current.walls[0] !== true) {
 
             current = grid[playerX + (playerY - 1) * cols];
             playerY -= 1;
        
+        // BOTTOM ARROW
         } else if (keyCode === DOWN_ARROW && playerY + 1 !== rows && (grid[index(cI, cJ + 1)].walls[0] !== true) && current.walls[2] !== true) {
 
             current = grid[playerX + (playerY + 1) * cols];
             playerY += 1;
             
         }
+
+        // CHECKS TO SEE IF THE PLAYER HAS WON!!!!
+
+        if (current === furthestCell) {
+            console.log('YOU WON!')
+        }
     }
 }
 
+
+
+
+// NIFTY LITTLE FUNCTION FOR FINDING THE INDEX OF A CELL IN A 1 DIMENSIONAL ARRAY
 function index(i, j) {
 
     if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
@@ -132,18 +195,27 @@ function index(i, j) {
 }
 
 
+
+// FUNCTION TO REMOVE THE CORRECT WALLS
+// TAKES PARAMETER (CURRENT, NEXT)
 function removeWalls(a, b) {
+
+    // CHECKS TO SEE IF THE NEXT CELL IS ABOVE, BELOW, LEFT, or RIGHT OF CURRENT CELL
+    // AND SETS THE CURRENT AND NEXT CELLS WALLS ARRAY TO FALSE AT CORRECT INDEX
+    // REPRESENTING EITHER THE TOP, RIGHT, BOTTOM, or LEFT WALLS FOR THOSE CELLS
 
     var x = a.i - b.i;
 
     var y = a.j - b.j;
 
+    // IF NEXT CELL IS TO THE RIGHT OF CURRENT CELL
     if (x === 1) {
 
         a.walls[3] = false;
 
         b.walls[1] = false;
 
+    // IF THE NEXT CELL IS TO THE LEFT OF CURRENT CELL
     } else if (x === -1) {
 
         a.walls[1] = false;
@@ -151,12 +223,15 @@ function removeWalls(a, b) {
         b.walls[3] = false;
 
     }
+
+    // IF THE NEXT CELL IS BELOW THE CURRENT CELL
     if (y === 1) {
 
         a.walls[0] = false;
 
         b.walls[2] = false;
 
+    // IF THE NEXT CELL IS ABOVE THE CURRENT CELL
     } else if (y === -1) {
 
         a.walls[2] = false;
